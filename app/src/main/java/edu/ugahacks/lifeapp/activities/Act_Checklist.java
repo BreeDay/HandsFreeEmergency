33c8f185674d66e46e1cf1e476b316883f7b6df9
mstiller7 <mstiller@ggc.edu>
2019-02-09 14:37:15 -0500

    App can play GIFs.

--- app/src/main/java/edu/ugahacks/lifeapp/activities/Act_Checklist.java
+++ app/src/main/java/edu/ugahacks/lifeapp/activities/Act_Checklist.java
@@ -4,16 +4,20 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import java.math.BigDecimal;
 import java.sql.Time;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 import edu.ugahacks.lifeapp.MainActivity;
 import edu.ugahacks.lifeapp.R;
@@ -37,8 +41,13 @@ public class Act_Checklist extends AppCompatActivity {
                 String alertText = "This is an automated alert message from " + appName + ".";
 
                 Location l = MainActivity.l;
-                double lat = l.getLatitude();
-                double lon = l.getLongitude();
+                if (l == null) {
+                    SystemClock.sleep(5000);
+                    Toast.makeText(getApplicationContext(), "Still obtaining location. Please wait a moment.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                double lat = Objects.requireNonNull(l).getLatitude();
+                double lon = Objects.requireNonNull(l).getLongitude();
 
                 alertText += " There is a victim located at (" + lat + ", " + lon + "). The victim is ";
 
@@ -82,7 +91,8 @@ public class Act_Checklist extends AppCompatActivity {
 
                 Log.d("sms", alertText);
 
-//                startActivity(new Intent()); // TODO: alert operators of conditions
+                // the Intent is to provide survivors with a sense of pride and accomplishment
+                startActivity(new Intent(getApplicationContext(), Act_GIF.class)); // TODO: alert operators of conditions
             }
         });
     }
---
+++ app/src/main/java/edu/ugahacks/lifeapp/activities/Act_GIF.java
@@ -0,0 +1,24 @@
+package edu.ugahacks.lifeapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import edu.ugahacks.lifeapp.R;
+
+public class Act_GIF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gif);
+
+        // TODO: insert relevant GIFs here instead of cats.
+        ImageView i = findViewById(R.id.anim_1);
+        Glide.with(getApplicationContext()).load("https://media.giphy.com/media/OmK8lulOMQ9XO/giphy.gif").into(i);
+
+    }
+}
