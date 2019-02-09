a533b734d775251080af1b7802097fd47601945b
mstiller7 <mstiller@ggc.edu>
2019-02-09 01:10:09 -0500

    Testing to see if I broke anything

--- app/src/main/java/edu/ugahacks/lifeapp/MainActivity.java
+++ app/src/main/java/edu/ugahacks/lifeapp/MainActivity.java
@@ -1,16 +1,33 @@
 package edu.ugahacks.lifeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.location.LocationServices;
 import com.ugahacks.lifeapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private c = LocationServices.getFusedLocationProviderClient(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button b = findViewById(R.id.call911);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent i = new Intent(this, SecondActivity.class);
+            }
+        });
+
     }
     //Test to see if git is working
+
 }
