03bff6364c4e3ea8480904b5bdb716cf99aa9463
Mansoor White <mwhite13@ggc.edu>
2019-02-09 20:44:54 -0500

    partial tts function

--- HandsFreeEmergency/src/main/java/edu/ugahacks/lifeapp/MainActivity.java
+++ HandsFreeEmergency/src/main/java/edu/ugahacks/lifeapp/MainActivity.java
@@ -6,15 +6,19 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.Task;
+
+import java.util.Locale;
 
 import edu.ugahacks.lifeapp.activities.Act_Checklist;
 
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Error";
     private FusedLocationProviderClient flpc;
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    SmsManager manager;
+    TextToSpeech tts;
     String phone;
     String txtMessage;
     public static Location l;
@@ -45,9 +51,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+           tts.speak("Is this an Emergency or Do you need to provide FirstAid? ",TextToSpeech.QUEUE_FLUSH,null,null);
         } else {
             flpc = LocationServices.getFusedLocationProviderClient(getApplicationContext());
             final Task t = flpc.getLastLocation();
@@ -55,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 // *should* be a Location.
                 l = (Location) o;
                 Toast.makeText(getApplicationContext(), "Location obtained!", Toast.LENGTH_SHORT).show();
+
             });
             Toast.makeText(getApplicationContext(), "Obtaining your location...", Toast.LENGTH_LONG).show();
         }
@@ -64,21 +73,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void smsText() {
-        txtMessage = "This is a Test";
-        phone = "(650)-555-1212";
+        phone = "4703518052";
 
         Intent messageIntent = new Intent(Intent.ACTION_SENDTO);
         messageIntent.setData(Uri.parse(phone));
         messageIntent.putExtra("Message", txtMessage);
         Log.i("Output is : ", txtMessage);
-        if (messageIntent.resolveActivity(getPackageManager()) != null) {
-            startActivity(messageIntent);
+        if(messageIntent.resolveActivity(getPackageManager()) != null){
+            //  startActivity(messageIntent);
+            manager.sendTextMessage(phone,null,txtMessage, null,null);
+
             Log.i("Is there a message?  ", txtMessage);
-        } else {
-            Log.d(TAG, "No message");
+        }else{
+            Log.d(TAG,"No message");
         }
 
     }
 
 
-}
+
+    }
+
+
+
